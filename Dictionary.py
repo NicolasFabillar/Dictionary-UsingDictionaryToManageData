1a0912c122458d6fa6891fbbc30574f8e28415bf
NicolasFabillar <nicosfabillar09@gmail.com>
2022-11-12 19:37:15 +0800

    Added the input statement with the choices, created a loop for the program and created functionality for choice 3 (Exit).
    git commit -, Added the input statement with the choices, created a loop for the program and created functionality for choice 3 (Exit).

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,11 @@
 print(f'{"*** Programmed by ***": ^40}')
 print(f'{"*** Nicolas Fabillar ***": ^40}\n')
 
-Choice = int(input("Menu: \n\t1 -> Add an item \n\t2 -> Search \n\t3 -> Exit (y/n)"))
+while True:
+    Choice = int(input("\nMenu: \n\t1 -> Add an item \n\t2 -> Search \n\t3 -> Exit (y/n) \nWhat do you want to do? (1-3): "))
+    if Choice == 3:
+        Exit = input("Do you want to exit? y/n: ")
+        if Exit == "y":
+            print("\n** Program Terminated **")
+            exit()
+        print("\nCancelled.")
