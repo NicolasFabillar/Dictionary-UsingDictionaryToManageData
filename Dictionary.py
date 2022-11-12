13fcf37153491a282d3a8e4fff6b32b90be1e5f2
NicolasFabillar <nicosfabillar09@gmail.com>
2022-11-12 19:30:26 +0800

    Added the menu options

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,4 @@
 print(f'{"*** Programmed by ***": ^40}')
 print(f'{"*** Nicolas Fabillar ***": ^40}\n')
 
-print()
+Choice = int(input("Menu: \n\t1 -> Add an item \n\t2 -> Search \n\t3 -> Exit (y/n)"))
