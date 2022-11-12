c1b89684ed48dbeb26f0d198d6adc5aeb1ddcabd
NicolasFabillar <nicosfabillar09@gmail.com>
2022-11-12 21:46:28 +0800

    Added a functionality where you can edit a contact, but not change the name. I also added a filtering function so that they can't choose in valid option in the main MENU. Also had minor changes and finished the program.

--- Dictionary.py
+++ Dictionary.py
@@ -11,45 +11,88 @@ PhoneBook = {
 }
 
 while True:
-    Choice = int(input("\nMenu: \n\t1 -> Add an item \n\t2 -> Search \n\t3 -> All Contacts \n\t4 -> Exit (y/n) \nWhat do you want to do? (1-4): "))
-    if Choice == 1:
-        FullName = input("Enter your full name: ")
-        Age = input("Enter your age: ")
-        Address = input("Enter your home address: ")
-        Number = input("Enter your phone number: ")
+    try:
+        Choice = int(input("\nMenu: \n\t1 -> Add an item \n\t2 -> Search \n\t3 -> All Contacts \n\t4 -> Edit Contact "
+                       "\n\t5 -> Exit (y/n) \nWhat do you want to do? (1-5): "))
+    except ValueError:
+        print("\n\tInvalid input, enter an integer.")
+    else:
+        if Choice == 1:
+            while True:
+                FullName = input("Enter full name: ")
+                Matches = 0
+                for key in PhoneBook:
+                    if key == FullName:
+                        print("Name already exists.\n")
+                        Matches = 1
+                if Matches == 0:
+                    break
 
-        # Putting the inputted data in a List named DataList.
-        DataList[0] = Age
-        DataList[1] = Address
-        DataList[2] = Number
+            Age = input("Enter age: ")
+            Address = input("Enter home address: ")
+            Number = input("Enter phone number: ")
 
-        # Putting the inputted data in a dictionary named PhoneBook with the key being the Full name.
-        PhoneBook[FullName] = DataList
-        print("Contact added.")
+            # Putting the inputted data in a List named DataList.
+            DataList[0] = Age
+            DataList[1] = Address
+            DataList[2] = Number
 
-    if Choice == 2:
-        SearchName = input("Enter the full name of the contact: ")
-        print("\n======================================= Phone Book =======================================")
-        print(f'{"Name": <33} {"Age": <10} {"Address": <33} {"Number": <15}')
-        Matches = 0
-        for key in PhoneBook:
-            if key == SearchName:
+            # Putting the inputted data in a dictionary named PhoneBook with the key being the Full name.
+            PhoneBook[FullName] = DataList
+            print("Contact added.")
+
+        if Choice == 2:
+            SearchName = input("Enter the full name of the contact: ")
+            print("\n========================================= Phone Book =========================================")
+            print(f'{"Name": <33} {"Age": <10} {"Address": <33} {"Contact Number": <15}')
+            Matches = 0
+            for key in PhoneBook:
+                if key == SearchName:
+                    print(f'{key.capitalize(): <33} {PhoneBook[key][0]: <10} {PhoneBook[key][1].capitalize(): <33} {PhoneBook[key][2]: <15}')
+                    Matches = 1
+            if Matches == 0:
+                print(f'\n{"*** No match found ***": ^94}\n')
+            print("==============================================================================================")
+
+        if Choice == 3:
+            print("\n========================================= Phone Book =========================================")
+            print(f'{"Name": <33} {"Age": <10} {"Address": <33} {"Contact Number": <15}')
+
+            for key in PhoneBook:
                 print(f'{key.capitalize(): <33} {PhoneBook[key][0]: <10} {PhoneBook[key][1].capitalize(): <33} {PhoneBook[key][2]: <15}')
-                Matches = 1
-        if Matches == 0:
-            print(f'\n{"*** No match found ***": ^86}')
+            print("\n==============================================================================================")
 
-    if Choice == 3:
-        print("\n======================================= Phone Book =======================================")
-        print(f'{"Name": <33} {"Age": <10} {"Address": <33} {"Number": <15}')
+        if Choice == 4:
+            ContactName = input("Enter the full name of contact you want to edit: ")
+            Matches = 0
+            for key in PhoneBook:
+                if key == ContactName:
+                    print("Match found! ")
+                    print("\tName:",key, "\n\tAge:", PhoneBook[key][0],
+                          "\n\tAddress:", PhoneBook[key][1].capitalize(), "\n\tContact Number:", PhoneBook[key][2])
+                    Matches = 1
+                    Change = input("\nDo you want to change this contact? y/n: ")
+                    if Change == "y":
+                        Age = input("Enter new age: ")
+                        Address = input("Enter home address: ")
+                        Number = input("Enter phone number: ")
+                        DataList[0] = Age
+                        DataList[1] = Address
+                        DataList[2] = Number
+                        del PhoneBook[key]
+                        PhoneBook[key] = DataList
+                        print("Contact edited.")
+                        Matches = 1
 
-        for key in PhoneBook:
-            print(f'{key.capitalize(): <33} {PhoneBook[key][0]: <10} {PhoneBook[key][1].capitalize(): <33} {PhoneBook[key][2]: <15}')
-        print("==========================================================================================")
+            if Matches == 0:
+                print("No match found.")
 
-    if Choice == 4:
-        Exit = input("Do you want to exit? y/n: ")
-        if Exit == "y":
-            print("\n** Program Terminated **")
-            exit()
-        print("\nCancelled.")
+        if Choice == 5:
+            Exit = input("Do you want to exit? y/n: ")
+            if Exit == "y":
+                print("\n** Program Terminated **")
+                exit()
+            print("\nCancelled.")
+
+        if Choice not in [1,2,3,4,5]:
+            print("\n\tInvalid input, out of range.")
